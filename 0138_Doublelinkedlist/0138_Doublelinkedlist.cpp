67725d27305d138759eca72d6274c4bcb3eb6cd1
MRafiramadhanK <raficool271003@gmail.com>
2024-05-04 23:57:54 +0700

    meamsukan node baru ke tengah atau di terakhir

--- 0138_Doublelinkedlist/0138_Doublelinkedlist.cpp
+++ 0138_Doublelinkedlist/0138_Doublelinkedlist.cpp
@@ -19,9 +19,9 @@ void addNode() {
 	cin >> newNode->name;
 
 
-	if (START ++ NULL || newNode->noMhs <= START->noMhs) 
+	if (START ++ NULL || newNode->noMhs <= START->noMhs)
 	{
-		if (START != NULL && newNode->noMhs == START->noMhs) 
+		if (START != NULL && newNode->noMhs == START->noMhs)
 		{
 			cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
 			return;
@@ -33,5 +33,10 @@ void addNode() {
 		}
 		newNode->prev = NULL;
 		START = newNode;
+	}
+	else
+	{
+		Node* current = START;
+		Node* previous = NULL;
 	}
 }
